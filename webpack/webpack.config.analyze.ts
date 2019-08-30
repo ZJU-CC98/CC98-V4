494bcee5c76d918d0e22060f35fa0a4d352085a8
AsukaSong <yansongdong@gmail.com>
2019-08-30 10:57:40 +0800

    fix: 调整 webpack 配置

---
+++ webpack/webpack.config.analyze.ts
@@ -0,0 +1,9 @@
+import merge from 'webpack-merge'
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer'
+import prod from './webpack.config.prod'
+
+const config = merge({}, prod, {
+  plugins: [new BundleAnalyzerPlugin()],
+})
+
+export default config
