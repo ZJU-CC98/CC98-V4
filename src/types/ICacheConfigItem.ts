3fd6cc1c32268539922d28c3b68cb52d5b9f85bc
AsukaSong <yansongdong@gmail.com>
2019-09-02 20:07:41 +0800

    feat: add cache

---
+++ src/types/ICacheConfigItem.ts
@@ -0,0 +1,35 @@
+import { AxiosResponse, AxiosRequestConfig } from 'axios'
+import UrlPattern from 'url-pattern'
+
+export type IdType = number | string
+
+export default interface ICacheConfigItem {
+  // 需要缓存的url，对应axios#config.url
+  // 可以直接在urlPattern中返回id
+  urlPath: string | RegExp | UrlPattern
+  // 全局唯一，对应indexedDb的objectStoreName
+  // 批量获取用户信息和单个获取应该用同一个namespace
+  namespace: string
+  // 过期时间，ms
+  expirationTime: number
+  // 内容的id，对应indexedDb的keyPath，支持path写法
+  // 默认 id
+  idKey?: string
+
+  // 从axios#config中获取到当前请求的query key
+  // 一般来说是资源的id
+  // 有时候也会是name，例如/user/name/:name接口
+  getRequestQueryKey?: (config: AxiosRequestConfig) => IdType[]
+
+  getDataFromResult: (result: AxiosResponse) => any[]
+
+  // 为缓存中不存在的id生成请求
+  // 默认直接使用当次请求的config
+  getFallbackConfig?: (missId: IdType[]) => AxiosRequestConfig
+
+  // 使用indexedDb的索引
+  // 不传是 store.get(queryKey)
+  // 传了是 store.index(useIndexName).get(queryKey)
+  useIndexName?: string
+  indexNames?: string[]
+}
---
+++ src/utils/indexedDb.ts
@@ -0,0 +1,95 @@
+import { uniqBy } from 'lodash'
+import ICacheConfigItem, { IdType } from 'src/types/ICacheConfigItem'
+
+export const shouldUseIndexedDb = !!window.indexedDB
+
+// don't export db, make it private
+let db: IDBDatabase | undefined
+
+const version = 1
+const dbName = 'cc98-v4'
+
+export function initIndexedDb(cacheConfigs: ICacheConfigItem[]) {
+  return new Promise(resolve => {
+    if (shouldUseIndexedDb && cacheConfigs.length > 0) {
+      const req = indexedDB.open(dbName, version)
+
+      req.onupgradeneeded = () => {
+        uniqBy(cacheConfigs, 'namespace')
+          .filter(({ namespace }) => !req.result.objectStoreNames.contains(namespace))
+          .forEach(({ namespace, indexNames }) => {
+            // 初始化
+            const store = req.result.createObjectStore(namespace)
+
+            if (indexNames) {
+              indexNames.forEach(indexName =>
+                store.createIndex(indexName, indexName, { unique: true })
+              )
+            }
+          })
+      }
+
+      req.onsuccess = () => {
+        db = req.result
+        resolve()
+      }
+      req.onerror = resolve
+    } else {
+      resolve()
+    }
+  })
+}
+
+export function query(
+  namespace: string,
+  id: IdType,
+  expirationTime: number,
+  useIndex?: string
+): Promise<any> {
+  if (!db) {
+    return Promise.reject(new Error('no db found'))
+  }
+
+  return new Promise<any>((resolve, reject) => {
+    const t = db!.transaction(namespace, 'readonly')
+    const store = t.objectStore(namespace)
+    const req = useIndex ? store.index(useIndex).get(id) : store.get(id)
+    req.onsuccess = () => {
+      if (req.result && req.result.addTime + expirationTime > Date.now()) {
+        resolve(req.result.content)
+      } else if (req.result) {
+        reject(new Error('expired'))
+      } else {
+        reject(new Error('not found'))
+      }
+    }
+
+    req.onerror = reject
+  })
+}
+
+export function queryAll(
+  namespace: string,
+  ids: IdType[],
+  expirationTime: number,
+  useIndex?: string
+): Promise<any[]> {
+  return Promise.all(ids.map(id => query(namespace, id, expirationTime, useIndex).catch(() => id)))
+}
+
+export function addItem(namespace: string, content: any) {
+  if (!db) {
+    return Promise.reject(new Error('db not fount'))
+  }
+
+  const t = db.transaction(namespace, 'readwrite')
+  const store = t.objectStore(namespace)
+  const req = store.put({
+    content,
+    addTime: Date.now(),
+  })
+  return new Promise((resolve, reject) => {
+    req.onsuccess = e => resolve(e)
+    req.onerror = e => reject(e)
+  }) as Promise<any>
+}
